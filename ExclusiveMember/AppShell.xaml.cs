0b19b99d3722e5763bb67eb89881d757921744d1
manjo <manjo@MANJOT-LAPTOP>
2024-04-18 19:57:17 -0600

    Add project files.

---
+++ ExclusiveMember/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExclusiveMember
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ExclusiveMember/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace ExclusiveMember
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        /*private void BuyBtnClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                BuyBtn.Text = $"Clicked {count} time";
+            else
+                BuyBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(BuyBtn.Text);
+        }*/
+    }
+
+}
